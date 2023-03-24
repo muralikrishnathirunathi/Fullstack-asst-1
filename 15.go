0894626c59880b0fd06f387ede9bff5d1bfd9428
muralikrishnathirunathi <116892036+muralikrishnathirunathi@users.noreply.github.com>
2023-03-24 18:31:29 +0530

    Create 15.go

---
+++ 15.go
@@ -0,0 +1,13 @@
+// Pi is a mathematical constant
+const Pi = 3.14
+
+func main() {
+	// World is a string constant in Chinese
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	// Truth is a boolean constant
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
