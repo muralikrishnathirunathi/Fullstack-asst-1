2f25a381cdd514df86c820ad6dfd596e3a927246
muralikrishnathirunathi <116892036+muralikrishnathirunathi@users.noreply.github.com>
2023-03-24 18:27:23 +0530

    Create 10.go

---
+++ 10.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, j := 4, 5
+	k := 6
+	c, python, java := false, true, "yes!"
+
+	fmt.Printf("i=%d\n", i)
+	fmt.Printf("j=%d\n", j)
+	fmt.Printf("k=%d\n", k)
+	fmt.Printf("c=%v\n", c)
+	fmt.Printf("python=%v\n", python)
+	fmt.Printf("java=%s\n", java)
+}
