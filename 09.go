ea9597d77370614638a790c2fe1c56b547195ef8
muralikrishnathirunathi <116892036+muralikrishnathirunathi@users.noreply.github.com>
2023-03-24 18:26:37 +0530

    Create 09.go

---
+++ 09.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, j := 3, 4
+	c, python, java := false, true, "yes!"
+	fmt.Printf("i=%d, j=%d, c=%v, python=%v, java=%s\n", i, j, c, python, java)
+}
