04667c456f1274dac8398817b8be208580c964b4
muralikrishnathirunathi <116892036+muralikrishnathirunathi@users.noreply.github.com>
2023-03-24 18:29:43 +0530

    Create 12.go

---
+++ 12.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 42
+	f := 3.14159265359
+	b := true
+	s := "Hello, World!"
+	fmt.Printf("i = %d\n", i)
+	fmt.Printf("f = %f\n", f)
+	fmt.Printf("b = %t\n", b)
+	fmt.Printf("s = %s\n", s)
+}
